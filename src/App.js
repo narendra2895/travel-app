2b84ab70a9e285552afda1c773a7a6754484422f
narendra2895 <106621535+narendra2895@users.noreply.github.com>
2024-05-06 03:05:08 +0100

    map fixe

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     const [places, setPlaces] = useState([]);
 
     const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
-    const [bounds, setBounds] = useState({ ne: null, sw: null });
+    const [bounds, setBounds] = useState({ ne: { lat: 0, lng: 0 }, sw: { lat: 0, lng: 0 } });
 
 
     useEffect(() => {
@@ -37,7 +37,7 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List places={places} />
+                    <List places={places}/>
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} />
